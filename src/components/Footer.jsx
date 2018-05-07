1676b4786e24796483ae641bd6dc2a15feef7ae5
Ameen Shaikh <34180338+AmeenShaikh025@users.noreply.github.com>
2018-05-07 15:19:51 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Footer.css';
+
+
+class Footer extends Component {
+    render(){
+        return(
+            <footer className="footer">
+                <div className="container">
+                    <span className="text-muted">ProjectReact &#169; {new Date().getFullYear()}</span>
+                </div>
+            </footer>
+        );
+    }
+}
+
+export default Footer;
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Navbar from '../components/Navbar.jsx';
+import Footer from '../components/Footer.jsx';
+import Jumbotron from '../components/Jumbotron.jsx'
+
+
+class Contact extends Component {
+    render(){
+        return(
+            <div>
+                <Navbar />
+                <Jumbotron title="Contact" subtitle="the Contact subtitle!" />
+                    <div className="container">
+                        <h2>Contact</h2>
+                        <p>Lorem ipsum dolor sit amet, per ignota aperiri forensibus an, vel ad illum clita fabulas. Qui et fuisset imperdiet, ex soleat meliore legendos pri. Ipsum lorem eu est, an ius ullum falli. Usu posse ipsum evertitur ea, dicit veniam debitis cu sit, mei legimus dissentiet in.</p>
+                        <p>Per aliquam nostrum an. At nemore malorum concludaturque eos. Id aliquip qualisque gubergren sit, sed diam docendi laboramus ea. Sale sonet incorrupte id est, essent admodum accusam cum an, eam ea antiopam sadipscing. Aeque recteque te est, ius modo stet omnesque at, an sea paulo alterum urbanitas.</p>
+                    </div>
+                <Footer />
+            </div>
+        );
+    }
+}
+
+export default Contact;
